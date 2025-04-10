2ac15a80962004bdcca1c29de916e02d6ead8331
Francis <ftangscipik@gmail.com>
2025-04-09 21:01:11 -0700

    Initial commit with bun, tailwind, and react-router

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+
+      <h1 className="text-5xl font-bold my-4 leading-tight">Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
